6b440d9d16b53707d321d9f4d6a567c7d33aea84
manjunath98v <v.manjunath@khoros.com>
2022-06-02 16:00:46 +0530

    weather app

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,5 @@
 export const SCHEDULER_PATH = '/kroo/scheduler';
 export const PAYMENT_PATH = "/kroo/payment";
 export const CAROUSEL_PATH = "/kroo/carousel";
+export const WEATHER_PATH = "/kroo/weather";
 
